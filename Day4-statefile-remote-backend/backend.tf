f6487e513873c22b7b5299f306849457cde1dc12
Kalpesh <kalpeshroy.cloud@gmail.com>
2026-09-18 01:00:34 +0530

    adding s3-dynamodb

--- Day4-statefile-remote-backend/backend.tf
+++ Day4-statefile-remote-backend/backend.tf
@@ -1,7 +1,10 @@
+# This backend configuration instructs Terraform to store its state in
 terraform {
   backend "s3" {
-    bucket = "thisbucketosforterraform"
-    key    = "Day4/terraform.tfstate"
-    region = "eu-north-1"
+    bucket         = "thisbucketosforterraformstatelock"              # Name of the S3 bucket where the state will be stored.
+    region         = "eu-north-1"                            # AWS region where the S3 bucket is located.
+    key            = "Day4/terraform.tfstate"             # Path within the bucket where the state will be read/written.
+    dynamodb_table = "terraform-state-lock-dynamo"    # DynamoDB table used for state locking, note: first run day-4-
+    encrypt        = true                             # Ensures the state is encrypted at rest in S3.
   }
 }
--- Day4-statefile-remote-backend/main.tf
+++ Day4-statefile-remote-backend/main.tf
@@ -12,3 +12,35 @@ resource "aws_s3_bucket" "name" {
     bucket = "bucketthanksyouterraform" 
   
 }
+
+
+
+
+# Challenges When Multiple Developers Use the Same State File
+
+# 1. Locking
+# Only one developer should modify the state at a time.
+
+# 2. Waiting
+# Other developers may have to wait while the state is locked.
+
+# 3. Concurrent Changes
+# Two developers may try to change the same resource.
+
+# 4. Accidental Changes
+# One developer's change can affect another developer's work.
+
+# 5. Accidental Deletion
+# One developer may delete a resource another developer needs.
+
+# 6. Coordination
+# Developers need to communicate before making major changes.
+
+# 7. State Corruption Risk
+# Incorrect manual changes to the state can affect everyone.
+
+# 8. Backend Dependency
+# If the remote backend is unavailable, all developers may be affected.
+
+
+#to overcome this we need to impliment state locking provision by using dynambo db
