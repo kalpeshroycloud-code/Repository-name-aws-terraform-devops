e2e32a402c2d7369556df6e793ce38727ee7d3ff
Kalpesh <kalpeshroy.cloud@gmail.com>
2026-09-17 19:09:05 +0530

    Add Day4 statefile local issues

---
+++ Day4-statefile-local-issues/main.tf
@@ -0,0 +1,10 @@
+resource "aws_instance" "name" {
+  ami               = "ami-03cc2fdb1443ab619"
+  instance_type     = "t3.micro"
+  key_name          = "outputkeypair"
+  availability_zone = "eu-north-1a"
+
+  tags = {
+    Name = "MyInstance_17-sep"
+  }
+}
