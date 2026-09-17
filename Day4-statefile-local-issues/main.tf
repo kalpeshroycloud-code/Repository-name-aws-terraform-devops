ccbef62d35546a88b699a3161488edbc41ef54d2
Kalpesh <kalpeshroy.cloud@gmail.com>
2026-09-17 21:04:20 +0530

    Update local statefile configuration

--- Day4-statefile-local-issues/main.tf
+++ Day4-statefile-local-issues/main.tf
@@ -8,3 +8,19 @@ resource "aws_instance" "name" {
     Name = "MyInstance_17-sep"
   }
 }
+
+# Both developers are managing the same AWS infrastructure,
+# but they have different Terraform state files.
+
+# Problems:
+# 1. Dev 1 creates an EC2 instance.
+# 2. Dev 2's state file does not know about Dev 1's EC2.
+# 3. Dev 2 may create another EC2 instance.
+# 4. Dev 1 changes something, but Dev 2 may not know about the change.
+# 5. One developer may accidentally change or delete a resource
+#    that is being used by the other developer.
+
+# In one line:
+# Different state files = different records of the same infrastructure
+# = possible conflicts and duplicate resources.
+#SOLUTION- MAINTAIN ONE STATE FILE FOR ALL DEVELOPERS TO USE.
